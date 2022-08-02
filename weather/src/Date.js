bcb4eb493f76855281c10c9f8b2e0471c3315ef4
DianaB177 <anaidb77@gmail.com>
2022-08-02 22:50:24 +0200

    degrees-commit

--- weather/src/Date.js
+++ weather/src/Date.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import "./Container.css";
 
 const Date = () => {
   return (
-    <div> <p>Wednesday 12:21</p></div>
+  <div className='Date'>
+     <p>Wednesday 12:21</p>
+     
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    
+    let day = days[date.getDay()];
+    let hour = date.getHours();
+
+    if (hour < 10) {
+      hour = `0${hour}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+   let dia =  `${day} ${hour}:${minutes}`;
+    return dia;
+  
+    </div>
   )
 }
 
