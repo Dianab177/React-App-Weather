5ec8e402e36cb7241d0b12f01a282c271832d7da
DianaB177 <anaidb77@gmail.com>
2022-08-03 00:03:46 +0200

    build-maked

---
+++ weather/src/Api.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Api = () => {
+    let apiKey = "094780c710fa4efd669f0df8c3991927";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; 
+}
+
+export default Api
+
+
